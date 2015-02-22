a7ced29607cef132b65cadb207583d01271a704d
Christoph Auer-Welsbach <awchristoph@gmail.com>
2015-02-22 19:31:23 +0100

    adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'bcards/index'
+  resources :bcards
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords", omniauth_callbacks: "users/omniauth_callbacks"}, skip: [:sessions, :registrations]
   devise_for :admin_users, ActiveAdmin::Devise.config
